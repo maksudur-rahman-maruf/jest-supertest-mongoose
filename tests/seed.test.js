c8a557268ca125aa15b7f90b909d09db466ccaba
maksudur-rahman-maruf <maruf.bsnm@gmail.com>
2022-01-11 17:34:39 +0600

    first commit

---
+++ tests/seed.test.js
@@ -0,0 +1,16 @@
+/* globals expect it */
+const User = require('../models/User')
+const { setupDB } = require('../test-setup')
+
+setupDB('seed', true)
+
+it('Seeding test', async () => {
+  const users = await User.find({})
+  expect(users.length).toBe(3)
+
+  // Make sure password is hashed
+  const firstUser = users[0]
+  const isCorrectlyHashed = firstUser.verifyPassword('12345678')
+  expect(isCorrectlyHashed).toBe(true)
+
+})
